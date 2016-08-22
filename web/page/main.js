a57b154741e9a5e55c85025e0a174ed4f1cfcec2
Charles Lohr <charles@cnlohr.com>
2016-08-22 00:52:14 -0400

    refs #7 - Make keyboard better.  If only we could figure out why it automatically capitolizes the first letter

--- web/page/main.js
+++ web/page/main.js
@@ -8,7 +8,7 @@ function KickHID()
 
 var lastMouseX;
 var lastMouseY;
-var TimeLastUp;
+var TimeLastUp, TimeLastDown;
 var ButtonsDown = 0;
 
 var UpTimeout;
@@ -16,7 +16,6 @@ var UpTimeout;
 
 function HandleUpDown( button, down )
 {
-	console.log( "HandleUpDown: " + button + " " + down );
 	var Mask = 1<<button;
 	if( down )
 		ButtonsDown |= Mask;
@@ -41,13 +40,15 @@ function MouseUpDown( down )
 	if( !down )
 	{
 		TimeLastUp = Date.now();
-		UpTimeout = setTimeout( function() { HandleUpDown( 0, false ); }, 200 );
+	    var dt = TimeLastUp - TimeLastDown;
+		if( dt < 100 ) HandleUpDown( 0, true );
+		UpTimeout = setTimeout( function() { HandleUpDown( 0, false ); }, 220 );
 	}
 	else
 	{
-		var now = Date.now();
-	    var dt = now - TimeLastUp;
-		HandleUpDown( 0, dt < 150 );
+		TimeLastDown = Date.now();
+	    var dt = TimeLastDown - TimeLastUp;
+		HandleUpDown( 0, dt < 130 );
 		try{ clearTimeout( UpTimeout ); } catch(e) { }
 	}
 }
@@ -57,7 +58,7 @@ function DeltaMouse( x, y )
 	msg = Math.round(x) + ", " + Math.round(y);
 	var msg = "CM" + ButtonsDown + "\t" + Math.round(x) + "\t" + Math.round(y);
 	QueueOperation( msg );	
-	console.log( msg );
+	//console.log( msg );
 }
 
 var KeyboardModifiers = 0;
@@ -66,7 +67,8 @@ var allUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+{}|:\"~<>?";
 var usblookup = {
     0x08 : 0x2A, // BACKSPACE
     0x09 : 0x2B, // TAB
-    0x0C : 0x28,
+    0x0C : 0x28, // also [enter]  (may not be needed)
+	0x0D : 0x28, // ENTER
     0x10 : 0xE1, // SHIFT
     0x11 : 0xE0, // CTRL
     0x12 : 0xE2, // ALT
@@ -88,9 +90,10 @@ for (var c = 0; c < 12; c++) {
     usblookup[s1[c].charCodeAt(0)] = c + 0x2D;
     usblookup[s2[c].charCodeAt(0)] = c + 0x2D;
 }
-for (var c = 0; c < 12; c++) { // F1 .. F12
-    usblookup[c + 0x70] = c + 0x3A;
-}
+//TODO: This would mask regular ASCII
+//for (var c = 0; c < 12; c++) { // F1 .. F12
+//    usblookup[c + 0x70] = c + 0x3A;
+//}
 //for (var c = 0; c < 12; c++) { // F13 .. F24
 //    usblookup[c + /* TODO: Figure out keycode for F13 */] = c + 0x68;
 //}
@@ -109,14 +112,25 @@ $(document).ready(function () {
             return;
         }
         var usbId = usblookup[c];
-        console.log(ev);
+        //console.log(ev);
         if (usbId === undefined) return; // Not a valid HID key code
         ev.preventDefault(); // Stop things like TAB switching fields
         this.value = "";
-        
+        //console.log( usbId + " " +  type + " " + ev.shiftKey );
         Keypress(usbId, type, ev.shiftKey);
     });
 });
+
+var keyops = [];
+function KeyInterval()
+{
+	if( keyops.length )
+	{
+		QueueOperation( keyops[0] );
+		keyops.shift();
+	}
+}
+setInterval( KeyInterval, 15 );
 
 function Keypress(id, type, shift) {
     if (shift) {
@@ -125,21 +139,21 @@ function Keypress(id, type, shift) {
         KeyboardModifiers &=~2;
     }
     
-    if (type == 1) QueueOperation("CK" + KeyboardModifiers + "\t" + id); // Need a sanity check;
-    if (type == 0) QueueOperation("CK0\t0");
+    if (type == 1) keyops.push("CK" + KeyboardModifiers + "\t" + id); // Need a sanity check;
+    if (type == 0) keyops.push("CK0\t0");
 }
 function KeypressMulti(str) {
     [].forEach.call(str, function(char) {
-        console.log(char);
+        //console.log(char);
         var modifier = 0;
         if (allUpper.indexOf(char) >= 0) modifier = 2;
         let id = usblookup[char.charCodeAt(0)];
         if (id === undefined) return;
         $("#typed").text(($("#typed").text() + char).slice(-50));
         $("#codes").text(($("#codes").text() + " " + ("0"+id.toString(16)).slice(-2)).slice(-50))
-        QueueOperation("CK" + modifier + "\t" + id);
+        keyops.push("CK" + modifier + "\t" + id);
+	    keyops.push("CK0\t0");
     });
-    QueueOperation("CK0\t0");
 }
 
 function AttachMouseListener()
@@ -183,79 +197,4 @@ function AttachMouseListener()
 }
 
 window.addEventListener("load", AttachMouseListener, false);
-
-/*
-is_leds_running = false;
-pause_led = false;
-
-function KickLEDs()
-{
-	$( "#LEDPauseButton" ).css( "background-color", (is_leds_running&&!pause_led)?"green":"red" );
-
-	if( !is_leds_running && !pause_led )
-		LEDDataTicker();
-
-}
-
-window.addEventListener("load", KickLEDs, false);
-
-function ToggleLEDPause()
-{
-	pause_led = !pause_led;
-	KickLEDs();
-}
-
-
-function GotLED(req,data)
-{
-	var ls = document.getElementById('LEDCanvasHolder');
-	var canvas = document.getElementById('LEDCanvas');
-	var ctx = canvas.getContext('2d');
-	var h = ls.height;
-	var w = ls.width;
-	if( canvas.width != ls.clientWidth-10 )   canvas.width = ls.clientWidth-10;
-	if( ctx.canvas.width != canvas.clientWidth )   ctx.canvas.width = canvas.clientWidth;
-
-	var secs = data.split( ":" );
-
-	$( "#LEDPauseButton" ).css( "background-color", "green" );
-
-	var samps = Number( secs[1] );
-	var data = secs[2];
-	var lastsamp = parseInt( data.substr(0,4),16 );
-	ctx.clearRect( 0, 0, canvas.width, canvas.height );
-
-	for( var i = 0; i < samps; i++ )
-	{
-		var x2 = i * canvas.clientWidth / samps;
-		var samp = data.substr(i*6,6);
-		var y2 = ( 1.-samp / 2047 ) * canvas.clientHeight;
-
-		ctx.fillStyle = "#" + samp.substr( 2, 2 ) + samp.substr( 0, 2 ) + samp.substr( 4, 2 );
-		ctx.lineWidth = 0;
-		ctx.fillRect( x2, 0, canvas.clientWidth / samps+1, canvas.clientHeight );
-	}
-
-	var samp = parseInt( data.substr(i*2,2),16 );
-
-	LEDDataTicker();
-} 
-
-function LEDDataTicker()
-{
-	if( IsTabOpen('LEDs') && !pause_led )
-	{
-		is_leds_running = true;
-		QueueOperation( "CL",  GotLED );
-	}
-	else
-	{
-		is_leds_running = 0;
-	}
-	$( "#LEDPauseButton" ).css( "background-color", (is_leds_running&&!pause_led)?"green":"red" );
-
-}
-
-*/
-
 
